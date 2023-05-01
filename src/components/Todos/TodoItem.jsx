33832a4a3631becbec0c834e75dd8663f25c7c98
dimasyrnyk <syrnykdmitriy@gmail.com>
2023-05-01 10:58:03 +0300

    added some styles to checkbox for better UI

--- src/components/Todos/TodoItem.jsx
+++ src/components/Todos/TodoItem.jsx
@@ -14,12 +14,16 @@ export default function TodoItem({ todo }) {
   return (
     <li className={"todo-item" + classes}>
       <div className="todo-item__container">
-        <input
-          className="todo-item__checkbox"
-          type="checkbox"
-          checked={todo.isCompleted}
-          onChange={hendleCheckbox}
-        />
+        <label class="todo-item__checkbox_label">
+          <input
+            className="todo-item__checkbox"
+            aria-label="an appropriate label"
+            type="checkbox"
+            checked={todo.isCompleted}
+            onChange={hendleCheckbox}
+          />
+        </label>
+
         <div className="todo-item__dates">
           <span>Created at: {todo.creationDate}</span>
           <span>Expired at: {todo.expirationDate}</span>
