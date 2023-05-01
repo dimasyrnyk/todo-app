90f37b81c580a5357752df3fa604b0aa182cf6a2
dimasyrnyk <syrnykdmitriy@gmail.com>
2023-05-01 12:42:24 +0300

    added trash icon button to todo item and logic to delete todo

--- src/store/todos/actions.js
+++ src/store/todos/actions.js
@@ -1,8 +1,13 @@
-import { ADD_TODO, TOGGLE_COMPLETE_TODO } from "./../types/todos";
+import { ADD_TODO, TOGGLE_COMPLETE_TODO, DELETE_TODO } from "./../types/todos";
 
 export const createTodo = (todo) => ({ type: ADD_TODO, payload: todo });
 
 export const completeTodo = (id) => ({
   type: TOGGLE_COMPLETE_TODO,
+  payload: id,
+});
+
+export const deleteTodo = (id) => ({
+  type: DELETE_TODO,
   payload: id,
 });
